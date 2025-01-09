b1de32a84f02e3558ab4b73a8b19d1d55a7b35bb
BacLuc <lucius.bachmann@gmx.ch>
2025-01-09 22:24:21 +0100

    api: set rfc_7807_compliant_errors to true
    It was a little tricky, but finally i figured out how to do it:
    https://api-platform.com/docs/core/errors/
    
    The big impact is, that the serialization of the errors is now
    done with the same infrastructure as serializing entities.
    That makes it less format dependent.
    
    It seems a little weird that we use a provider to "replace" the exception path
    of the response by extracting the exception to the context,
    and then continuing like when we would serialize a response.
    But i can also see the benefits.
    See https://github.com/api-platform/core/pull/5974
    
    Issue: #6618

---
+++ api/src/Serializer/Normalizer/ValidationErrorNormalizer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Serializer\Normalizer;
+
+use App\DTO\ValidationError;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class ValidationErrorNormalizer implements NormalizerInterface {
+    public const TYPE = 'type';
+    public const TITLE = 'title';
+    private array $defaultContext = [
+        self::TYPE => 'https://tools.ietf.org/html/rfc2616#section-10',
+        self::TITLE => 'An error occurred',
+    ];
+
+    public function normalize(mixed $data, ?string $format = null, array $context = []): null|array|\ArrayObject|bool|float|int|string {
+        return [
+            'type' => $context[self::TYPE] ?? $this->defaultContext[self::TYPE],
+            'title' => $context[self::TITLE] ?? $this->defaultContext[self::TITLE],
+            'detail' => $data->getDetail(),
+            'violations' => $data->getViolations(),
+        ];
+    }
+
+    public function supportsNormalization(mixed $data, ?string $format = null, array $context = []): bool {
+        return $data instanceof ValidationError;
+    }
+
+    public function getSupportedTypes(?string $format): array {
+        return [ValidationError::class => true];
+    }
+}
