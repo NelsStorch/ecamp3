d1752c23b8db6dda9b29e7cec93f01ac68088fce
BacLuc <lucius.bachmann@gmx.ch>
2025-03-09 18:28:43 +0100

    e2e: extract variables in content-types.cy.js
    And remove the id from the item test name,
    it does not help with debugging or make the test clearer.

--- e2e/specs/httpCache/content-types.cy.js
+++ e2e/specs/httpCache/content-types.cy.js
@@ -1,19 +1,21 @@
 import collectionResponse from './responses/content_types_collection.json'
 import itemResponse from './responses/content_types_entity.json'
+import { bipiUser, cachedEndpoint, castorUser } from '../constants'
+
+const collectionXKeys =
+  'a4211c11211c c462edd869f3 5e2028c55ee4 a4211c112939 f17470519474 1a0f84e322c8 3ef17bd1df72 4f0c657fecef 44dcc7493c65 cfccaecd4bad 318e064ea0c9 /api/content_types'
 
 describe('cache test: /content-types', () => {
   it('caches collection separately for each login', () => {
     const uri = '/api/content_types'
 
     Cypress.session.clearAllSavedSessions()
-    cy.login('test@example.com')
+    cy.login(bipiUser)
 
     // first request is a cache miss
-    cy.request(Cypress.env('API_ROOT_URL_CACHED') + uri + '.jsonhal').then((response) => {
+    cy.request(`${cachedEndpoint}${uri}.jsonhal`).then((response) => {
       const headers = response.headers
-      expect(headers.xkey).to.eq(
-        'a4211c11211c c462edd869f3 5e2028c55ee4 a4211c112939 f17470519474 1a0f84e322c8 3ef17bd1df72 4f0c657fecef 44dcc7493c65 cfccaecd4bad 318e064ea0c9 /api/content_types'
-      )
+      expect(headers.xkey).to.eq(collectionXKeys)
       expect(headers['x-cache']).to.eq('MISS')
       expect(response.body).to.deep.equal(collectionResponse)
     })
@@ -22,20 +24,21 @@ describe('cache test: /content-types', () => {
     cy.expectCacheHit(uri)
 
     // request with a new user is a cache miss
-    cy.login('castor@example.com')
+    cy.login(castorUser)
     cy.expectCacheMiss(uri)
   })
 
-  it('caches item 318e064ea0c9', () => {
-    const uri = '/api/content_types/318e064ea0c9'
+  it('caches item', () => {
+    const contentTypeId = '318e064ea0c9'
+    const uri = `/api/content_types/${contentTypeId}`
 
     Cypress.session.clearAllSavedSessions()
-    cy.login('test@example.com')
+    cy.login(bipiUser)
 
     // first request is a cache miss
-    cy.request(Cypress.env('API_ROOT_URL_CACHED') + uri + '.jsonhal').then((response) => {
+    cy.request(`${cachedEndpoint}${uri}.jsonhal`).then((response) => {
       const headers = response.headers
-      expect(headers.xkey).to.eq('318e064ea0c9')
+      expect(headers.xkey).to.eq(contentTypeId)
       expect(headers['x-cache']).to.eq('MISS')
       expect(response.body).to.deep.equal(itemResponse)
     })
