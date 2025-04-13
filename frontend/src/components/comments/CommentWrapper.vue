f3d414e16d636afdd67140a9d3e6395f359a5ae5
Manuel Meister <news.manuelsworld@gmail.com>
2025-04-13 21:03:38 +0200

    Add slot

--- frontend/src/components/comments/CommentWrapper.vue
+++ frontend/src/components/comments/CommentWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="display: flex" class="gap-4 flex-wrap">
+  <div style="display: flex" class="gap-4">
     <div style="flex-grow: 1; flex-shrink: 1">
       <slot />
     </div>
@@ -8,67 +8,22 @@
       style="flex-basis: 320px"
       class="d-flex flex-column gap-2 items-center"
     >
-      <Comment deletable>
-        <p class="mb-2">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi magnam nobis
-          perferendis rerum sequi. Culpa cupiditate dolorum, ea earum eveniet harum illo,
-          illum nemo neque pariatur quasi quia.
-        </p>
-        <div class="d-flex align-center justify-space-between mt-1 gap-2">
-          <span
-            ><UserAvatar :user="authUser" size="24" class="mr-1" />
-            {{ authUser.displayName }}</span
-          >
-          <span>31.02.2025 12:00</span>
-        </div>
-      </Comment>
-      <Comment class="relative">
-        <e-textarea dense placeholder="Kommentar" style="margin: -8px -12px 0" />
-        <div class="d-flex align-center mt-2 gap-2">
-          <span
-            ><UserAvatar :user="authUser" size="24" class="mr-1" />
-            {{ authUser.displayName }}</span
-          >
-        </div>
-        <v-btn absolute text style="bottom: 2px; right: 1px">Send</v-btn>
-      </Comment>
+      <slot name="comments" />
     </div>
     <v-navigation-drawer
       v-else
-      v-model="openComments"
+      :value="openComments ? null : false"
       clipped
       right
       app
       permanent
-      :temporary="$vuetify.breakpoint.smAndDown"
-      :color="$vuetify.breakpoint.smAndDown ? 'blue-grey' : 'transparent'"
+      temporary
+      color="blue-grey"
       floating
+      width="320"
     >
-      <div class="py-3 pr-3 pl-3 pl-md-0 d-flex flex-column gap-2 items-center">
-        <Comment deletable>
-          <p class="mb-2">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi magnam nobis
-            perferendis rerum sequi. Culpa cupiditate dolorum, ea earum eveniet harum
-            illo, illum nemo neque pariatur quasi quia.
-          </p>
-          <div class="d-flex align-center justify-space-between mt-1 gap-2">
-            <span
-              ><UserAvatar :user="authUser" size="24" class="mr-1" />
-              {{ authUser.displayName }}</span
-            >
-            <span>31.02.2025 12:00</span>
-          </div>
-        </Comment>
-        <Comment class="relative">
-          <e-textarea dense placeholder="Kommentar" style="margin: -8px -12px 0" />
-          <div class="d-flex align-center mt-2 gap-2">
-            <span
-              ><UserAvatar :user="authUser" size="24" class="mr-1" />
-              {{ authUser.displayName }}</span
-            >
-          </div>
-          <v-btn absolute text style="bottom: 2px; right: 1px">Send</v-btn>
-        </Comment>
+      <div class="py-3 px-3 d-flex flex-column gap-2 items-center">
+        <slot name="comments" />
       </div>
     </v-navigation-drawer>
     <v-btn
@@ -86,21 +41,12 @@
 </template>
 
 <script>
-import UserAvatar from '@/components/user/UserAvatar.vue'
-import { mapGetters } from 'vuex'
-
 export default {
   name: 'CommentWrapper',
-  components: { UserAvatar },
   data() {
     return {
       openComments: false,
     }
-  },
-  computed: {
-    ...mapGetters({
-      authUser: 'getLoggedInUser',
-    }),
   },
 }
 </script>
--- frontend/src/views/camp/activity/Activity.vue
+++ frontend/src/views/camp/activity/Activity.vue
@@ -6,6 +6,32 @@ Displays a single activity
   <v-container fluid>
     <CommentWrapper>
       <ScheduleEntry :activity-id="activityId" :schedule-entry-id="scheduleEntryId" />
+      <template #comments>
+        <Comment deletable>
+          <p class="mb-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi magnam nobis
+            perferendis rerum sequi. Culpa cupiditate dolorum, ea earum eveniet harum
+            illo, illum nemo neque pariatur quasi quia.
+          </p>
+          <div class="d-flex align-center justify-space-between mt-1 gap-2">
+            <span
+              ><UserAvatar :user="authUser" size="24" class="mr-1" />
+              {{ authUser.displayName }}</span
+            >
+            <span>31.02.2025 12:00</span>
+          </div>
+        </Comment>
+        <Comment class="relative">
+          <e-textarea dense placeholder="Kommentar" style="margin: -8px -12px 0" />
+          <div class="d-flex align-center mt-2 gap-2">
+            <span
+              ><UserAvatar :user="authUser" size="24" class="mr-1" />
+              {{ authUser.displayName }}</span
+            >
+          </div>
+          <v-btn absolute text style="bottom: 2px; right: 1px">Send</v-btn>
+        </Comment>
+      </template>
     </CommentWrapper>
   </v-container>
 </template>
@@ -13,10 +39,13 @@ Displays a single activity
 <script>
 import ScheduleEntry from '@/components/activity/ScheduleEntry.vue'
 import CommentWrapper from '@/components/comments/CommentWrapper.vue'
+import UserAvatar from '@/components/user/UserAvatar.vue'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'Activity',
   components: {
+    UserAvatar,
     CommentWrapper,
     ScheduleEntry,
   },
@@ -30,6 +59,11 @@ export default {
       type: String,
       default: null,
     },
+  },
+  computed: {
+    ...mapGetters({
+      authUser: 'getLoggedInUser',
+    }),
   },
 }
 </script>
