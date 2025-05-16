ff32c63e5d724756974ee31f792c18341adcc69a
carlobeltrame <carlo@beltra.me>
2025-05-16 18:05:49 +0200

    Indicate when filter is active

--- frontend/src/components/print/config/DialogScheduleEntryFilter.vue
+++ frontend/src/components/print/config/DialogScheduleEntryFilter.vue
@@ -9,12 +9,14 @@
       <v-chip
         :input-value="dialogOpen"
         outlined
+        :color="anyFilter ? 'info' : ''"
         class="align-self-center mt-4"
         v-bind="attrs"
         v-on="on"
       >
         <v-icon left size="20">mdi-filter</v-icon>
-        {{ $tc('components.print.config.dialogScheduleEntryFilter.filterActivities') }}
+        {{ activatorLabel }}
+        <CountBadge v-if="anyFilter" :count="filterFn(filter).length" />
       </v-chip>
     </template>
     <v-card>
@@ -29,10 +31,11 @@
 </template>
 <script>
 import ScheduleEntryFilters from '../../program/ScheduleEntryFilters.vue'
+import CountBadge from '../../dashboard/CountBadge.vue'
 
 export default {
   name: 'DialogScheduleEntryFilter',
-  components: { ScheduleEntryFilters },
+  components: { CountBadge, ScheduleEntryFilters },
   props: {
     camp: {},
     filterFn: {},
@@ -42,6 +45,18 @@ export default {
     return {
       dialogOpen: false,
       localFilter: this.filter,
+    }
+  },
+  computed: {
+    activatorLabel() {
+      if (this.anyFilter) return this.$tc('components.print.config.dialogScheduleEntryFilter.filterActive')
+      return this.$tc('components.print.config.dialogScheduleEntryFilter.filterActivities')
+    },
+    anyFilter() {
+      return this.filter.period ||
+        (this.filter.responsible != null && this.filter.responsible.length > 0) ||
+        (this.filter.category != null && this.filter.category.length > 0) ||
+        (this.filter.progressLabel != null && this.filter.progressLabel.length > 0)
     }
   },
   methods: {
