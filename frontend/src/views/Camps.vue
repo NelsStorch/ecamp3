e901fefd02013bb86c51a31e2c3bd0bdf649e71c
carlobeltrame <carlo@beltra.me>
2025-08-11 21:24:41 +0200

    Reverse time ordering for past camps

--- frontend/src/views/Camps.vue
+++ frontend/src/views/Camps.vue
@@ -132,7 +132,9 @@ export default {
     pastCamps() {
       return Object.values(
         groupBy(
-          this.periods.items.filter((p) => !dayjs(p.end).endOf('day').isAfter(dayjs())),
+          this.periods.items
+            .filter((p) => !dayjs(p.end).endOf('day').isAfter(dayjs()))
+            .sort((p1, p2) => dayjs(p2.start).unix() - dayjs(p1.start).unix()),
           (p) => p.camp()._meta.self
         )
       )
