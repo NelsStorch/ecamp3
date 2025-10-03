a872bad78bc6e773f6258ce5c64b16b09b36eb8b
Pirmin Mattmann <pimattmann@gmail.com>
2025-10-03 23:12:35 +0200

    rename migration

--- api/migrations/schema/Version20251003000000.php
+++ api/migrations/schema/Version20251003000000.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250811195334 extends AbstractMigration {
+final class Version20251003000000 extends AbstractMigration {
     public function getDescription(): string {
         return 'Done-Column for MaterialItem';
     }
