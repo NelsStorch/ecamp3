34c3d9f55370c73882fb50c2e9be3ecdc31f2840
Samuel Imfeld <samuel.imfeld@autodesk.com>
2025-10-03 23:15:42 +0200

    Extract into separate component

--- frontend/src/views/camp/activity/Activity.vue
+++ frontend/src/views/camp/activity/Activity.vue
@@ -9,75 +9,22 @@ Displays a single activity
       :activity-id="activityId"
       :schedule-entry-id="scheduleEntryId"
     />
-    <CommentWrapper v-else-if="!comments._meta.loading">
+    <Comments v-else :activity-id="activityId">
       <ScheduleEntry :activity-id="activityId" :schedule-entry-id="scheduleEntryId" />
-      <template #comments>
-        <Comment
-          v-for="comment in comments.items"
-          :key="comment._meta.self"
-          deletable
-          :comment="comment"
-        >
-          <e-richtext
-            class="e-story-day e-story-day-readonly"
-            :value="comment.textHtml"
-            :readonly="true"
-            :outlined="false"
-            :solo="false"
-            auto-grow
-            dense
-            label=""
-          />
-          <div class="d-flex align-center justify-space-between mt-1 gap-2">
-            <span
-              ><UserAvatar :user="comment.author()" size="24" class="mr-1" />
-              {{ comment.author().displayName }}</span
-            >
-            <span>{{
-              $date(comment.createTime).format($tc('global.datetime.dateTimeLong'))
-            }}</span>
-          </div>
-        </Comment>
-        <Comment class="relative">
-          <e-richtext
-            class="e-story-day e-story-day--textmode"
-            :value="newComment"
-            placeholder="Kommentar"
-            @input="onInput"
-          />
-          <div class="d-flex align-center mt-2 gap-2">
-            <span
-              ><UserAvatar :user="authUser" size="24" class="mr-1" />
-              {{ authUser.displayName }}</span
-            >
-          </div>
-          <v-btn
-            absolute
-            text
-            style="bottom: 2px; right: 1px"
-            :disabled="newComment.length === 0"
-            @click="addComment"
-            >Send</v-btn
-          >
-        </Comment>
-      </template>
-    </CommentWrapper>
+    </Comments>
   </v-container>
 </template>
 
 <script>
 import ScheduleEntry from '@/components/activity/ScheduleEntry.vue'
-import CommentWrapper from '@/components/comments/CommentWrapper.vue'
-import UserAvatar from '@/components/user/UserAvatar.vue'
+import Comments from '@/components/comments/Comments.vue'
 import { getEnv } from '@/environment.js'
-import { mapGetters } from 'vuex'
 
 export default {
   name: 'Activity',
   components: {
-    UserAvatar,
-    CommentWrapper,
     ScheduleEntry,
+    Comments,
   },
   props: {
     activityId: {
@@ -89,52 +36,10 @@ export default {
       default: null,
     },
   },
-  data() {
-    return {
-      newComment: '',
-    }
-  },
   computed: {
-    ...mapGetters({
-      authUser: 'getLoggedInUser',
-    }),
-    comments() {
-      return this.api.get().activities({ id: this.activityId }).comments()
-    },
     featureCommentsEnabled() {
       return getEnv().FEATURE_COMMENTS ?? false
     },
   },
-  methods: {
-    onInput(newValue) {
-      this.newComment = newValue
-    },
-    async addComment() {
-      const activity = this.api.get().activities({ id: this.activityId })
-      await this.api.post(await this.api.href(this.api.get(), 'comments'), {
-        textHtml: this.newComment,
-        activity: activity._meta.self,
-        camp: activity.camp()._meta.self,
-      })
-      await this.comments.$reload()
-      this.newComment = ''
-    },
-  },
 }
 </script>
-
-<style scoped>
-.e-story-day :deep(.v-text-field) {
-  margin-top: 0;
-  padding-top: 0;
-}
-
-.e-story-day.e-story-day-readonly :deep(.v-input__slot) {
-  padding: 0 !important;
-}
-
-/* this disables the bottom border which is displayed for VTextField in "regular" style */
-.e-story-day--textmode :deep(.v-input__slot)::before {
-  display: none;
-}
-</style>
