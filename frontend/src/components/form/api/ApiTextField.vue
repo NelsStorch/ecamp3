e788bc10ef62fb099cf7f5a9f04b0b0937411b17
Urban Suppiger <urban@suppiger.net>
2025-10-04 19:36:49 +0200

    fix ApiWrapper listeners

--- frontend/src/components/form/api/ApiTextField.vue
+++ frontend/src/components/form/api/ApiTextField.vue
@@ -3,7 +3,7 @@ Displays a field as a e-text-field + write access via API wrapper
 -->
 
 <template>
-  <api-wrapper v-slot="wrapper" v-bind="$props">
+  <api-wrapper v-slot="wrapper" v-bind="{ ...$props, ...$attrs }">
     <e-text-field
       ref="textField"
       :value="wrapper.localValue"
