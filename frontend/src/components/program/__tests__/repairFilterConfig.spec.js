1d894d7b21434eda9c134ecbba327f632adf0479
carlobeltrame <carlo@beltra.me>
2025-10-04 16:29:57 +0200

    Don't clean up filter attributes while they are still loading

--- frontend/src/components/program/__tests__/repairFilterConfig.spec.js
+++ frontend/src/components/program/__tests__/repairFilterConfig.spec.js
@@ -6,10 +6,12 @@ describe('repairFilterConfig', () => {
     _meta: { self: '/camps/1a2b3c4d' },
     shortTitle: 'test camp',
     periods: () => ({
+      _meta: { loading: false },
       items: [
         {
           _meta: { self: '/periods/1a2b3c4d' },
           days: () => ({
+            _meta: { loading: false },
             items: [
               {
                 _meta: { self: '/days/1a2b3c4d' },
@@ -20,6 +22,7 @@ describe('repairFilterConfig', () => {
       ],
     }),
     categories: () => ({
+      _meta: { loading: false },
       items: [
         {
           _meta: { self: '/categories/1a2b3c4d' },
@@ -27,6 +30,7 @@ describe('repairFilterConfig', () => {
       ],
     }),
     campCollaborations: () => ({
+      _meta: { loading: false },
       items: [
         {
           _meta: { self: '/camp_collaborations/1a2b3c4d' },
@@ -34,6 +38,7 @@ describe('repairFilterConfig', () => {
       ],
     }),
     progressLabels: () => ({
+      _meta: { loading: false },
       items: [
         {
           _meta: { self: '/progress_labels/1a2b3c4d' },
@@ -113,6 +118,36 @@ describe('repairFilterConfig', () => {
     expect(result).toEqual(defaultFilter)
   })
 
+  test('ignores invalid period if periods are still loading', () => {
+    // given
+    const config = {
+      period: '/periods/00000000',
+      day: [],
+      category: [],
+      progressLabel: [],
+      responsible: [],
+      activityCount: 0,
+    }
+
+    // when
+    const result = repairFilterConfig(config, {
+      ...camp,
+      periods: () => ({
+        _meta: { loading: true },
+      }),
+    })
+
+    // then
+    expect(result).toEqual({
+      period: '/periods/00000000',
+      day: [],
+      category: [],
+      progressLabel: [],
+      responsible: [],
+      activityCount: 0,
+    })
+  })
+
   test('adds responsible filter when missing', () => {
     // given
     const config = {
@@ -146,6 +181,36 @@ describe('repairFilterConfig', () => {
 
     // then
     expect(result).toEqual(defaultFilter)
+  })
+
+  test('ignores invalid responsible if campCollaborations are still loading', () => {
+    // given
+    const config = {
+      period: null,
+      day: [],
+      category: [],
+      progressLabel: [],
+      responsible: ['/camp_collaborations/00000000'],
+      activityCount: 0,
+    }
+
+    // when
+    const result = repairFilterConfig(config, {
+      ...camp,
+      campCollaborations: () => ({
+        _meta: { loading: true },
+      }),
+    })
+
+    // then
+    expect(result).toEqual({
+      period: null,
+      day: [],
+      category: [],
+      progressLabel: [],
+      responsible: ['/camp_collaborations/00000000'],
+      activityCount: 0,
+    })
   })
 
   test('adds category filter when missing', () => {
@@ -183,6 +248,36 @@ describe('repairFilterConfig', () => {
     expect(result).toEqual(defaultFilter)
   })
 
+  test('ignores invalid category when categories are still loading', () => {
+    // given
+    const config = {
+      period: null,
+      day: [],
+      category: ['/categories/00000000'],
+      progressLabel: [],
+      responsible: [],
+      activityCount: 0,
+    }
+
+    // when
+    const result = repairFilterConfig(config, {
+      ...camp,
+      categories: () => ({
+        _meta: { loading: true },
+      }),
+    })
+
+    // then
+    expect(result).toEqual({
+      period: null,
+      day: [],
+      category: ['/categories/00000000'],
+      progressLabel: [],
+      responsible: [],
+      activityCount: 0,
+    })
+  })
+
   test('adds day filter when missing', () => {
     // given
     const config = {
@@ -218,6 +313,74 @@ describe('repairFilterConfig', () => {
     expect(result).toEqual(defaultFilter)
   })
 
+  test('removes invalid day when days are still loading', () => {
+    // given
+    const config = {
+      period: null,
+      day: ['/days/00000000'],
+      category: [],
+      progressLabel: [],
+      responsible: [],
+      activityCount: 0,
+    }
+
+    // when
+    const result = repairFilterConfig(config, {
+      ...camp,
+      periods: () => ({
+        _meta: { loading: false },
+        items: [
+          {
+            _meta: { self: '/periods/1a2b3c4d' },
+            days: () => ({
+              _meta: { loading: true },
+            }),
+          },
+        ],
+      }),
+    })
+
+    // then
+    expect(result).toEqual({
+      period: null,
+      day: ['/days/00000000'],
+      category: [],
+      progressLabel: [],
+      responsible: [],
+      activityCount: 0,
+    })
+  })
+
+  test('removes invalid day when periods are still loading', () => {
+    // given
+    const config = {
+      period: null,
+      day: ['/days/00000000'],
+      category: [],
+      progressLabel: [],
+      responsible: [],
+      activityCount: 0,
+    }
+
+    // when
+    const result = repairFilterConfig(config, {
+      ...camp,
+      periods: () => ({
+        _meta: { loading: true },
+      }),
+    })
+
+    // then
+    expect(result).toEqual({
+      period: null,
+      day: ['/days/00000000'],
+      category: [],
+      progressLabel: [],
+      responsible: [],
+      activityCount: 0,
+    })
+  })
+
   test('adds progressLabel filter when missing', () => {
     // given
     const config = {
@@ -251,6 +414,36 @@ describe('repairFilterConfig', () => {
 
     // then
     expect(result).toEqual(defaultFilter)
+  })
+
+  test('ignores invalid progressLabel when progressLabels are still loading', () => {
+    // given
+    const config = {
+      period: null,
+      day: [],
+      category: [],
+      progressLabel: ['/progress_labels/00000000'],
+      responsible: [],
+      activityCount: 0,
+    }
+
+    // when
+    const result = repairFilterConfig(config, {
+      ...camp,
+      progressLabels: () => ({
+        _meta: { loading: true },
+      }),
+    })
+
+    // then
+    expect(result).toEqual({
+      period: null,
+      day: [],
+      category: [],
+      progressLabel: ['/progress_labels/00000000'],
+      responsible: [],
+      activityCount: 0,
+    })
   })
 
   test('adds dummy activityCount if missing', () => {
