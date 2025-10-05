d40857a6a4a5d7e3a55c9e3e01430ce20909d6ea
Manuel Meister <news.manuelsworld@gmail.com>
2025-10-05 04:02:18 +0200

    style: fix admin info sharing

--- frontend/src/components/campAdmin/CampSharingSettings.vue
+++ frontend/src/components/campAdmin/CampSharingSettings.vue
@@ -4,80 +4,80 @@
     icon="mdi-earth"
   >
     <v-skeleton-loader v-if="camp._meta.loading" type="article" />
-    <v-list class="py-0" color="transparent" two-line>
+    <v-list class="py-0" color="transparent" lines="two">
       <v-list-item class="px-0">
-        <v-list-item-content>
-          <v-list-item-title>
-            {{ $t(`components.campAdmin.campSharingSettings.${sharingStatus}.title`) }}
-          </v-list-item-title>
-          <v-list-item-subtitle v-if="camp.isShared" class="pb-1 whitespace-normal">
-            {{
-              $t('components.campAdmin.campSharingSettings.sharedSince', 1, {
-                sharedSince,
-                sharedBy,
-              })
-            }}
-          </v-list-item-subtitle>
-        </v-list-item-content>
-        <v-list-item-action>
-          <DialogShare :title="$t('components.campAdmin.campSharingSettings.title')">
-            <template #activator="{ on, attrs }">
-              <ButtonEdit
-                color="primary--text"
-                text
-                class="my-n1 v-btn--has-bg"
-                icon="mdi-earth"
-                v-bind="attrs"
-                v-on="on"
-                >{{
-                  camp.isShared ? $t('global.button.edit') : $t('global.button.share')
-                }}</ButtonEdit
-              >
-            </template>
-            <p>
-              <strong>{{
-                $t(`components.campAdmin.campSharingSettings.${sharingStatus}.title`)
-              }}</strong
-              >{{
-                $t(
-                  `components.campAdmin.campSharingSettings.${sharingStatus}.description`
-                )
-              }}
-            </p>
-            <p>
-              <v-btn
-                v-if="isManager"
-                :color="camp.isShared ? '' : 'error'"
-                elevation="0"
-                text
-                class="v-btn--has-bg"
-                :loading="loading"
-                @click="toggleShare"
-                ><v-icon start>mdi-alert</v-icon
-                >{{
-                  camp.isShared
-                    ? $t('components.campAdmin.campSharingSettings.deactivate')
-                    : $t('components.campAdmin.campSharingSettings.activate')
-                }}</v-btn
-              >
-            </p>
-            <p v-if="!camp.isShared">
-              {{ $t('components.campAdmin.campSharingSettings.implications') }}
-            </p>
-            <template v-if="camp.isShared" #more-actions>
-              <div class="d-flex gap-2 align-center w-100">
-                <small class="flex-shrink-1 flex-grow-1 w-0"
-                  ><a :href="campUrl">{{ campUrl }}</a></small
+        <v-list-item-title>
+          {{ $t(`components.campAdmin.campSharingSettings.${sharingStatus}.title`) }}
+        </v-list-item-title>
+        <v-list-item-subtitle v-if="camp.isShared" class="pb-1 whitespace-normal">
+          {{
+            $t('components.campAdmin.campSharingSettings.sharedSince', 1, {
+              sharedSince,
+              sharedBy,
+            })
+          }}
+        </v-list-item-subtitle>
+        <template #append>
+          <v-list-item-action>
+            <DialogShare :title="$t('components.campAdmin.campSharingSettings.title')">
+              <template #activator="{ on, attrs }">
+                <ButtonEdit
+                  color="primary"
+                  variant="tonal"
+                  class="my-n1"
+                  icon="mdi-earth"
+                  v-bind="attrs"
+                  v-on="on"
+                  >{{
+                    camp.isShared ? $t('global.button.edit') : $t('global.button.share')
+                  }}</ButtonEdit
                 >
+              </template>
+              <p>
+                <strong>{{
+                  $t(`components.campAdmin.campSharingSettings.${sharingStatus}.title`)
+                }}</strong
+                >{{
+                  $t(
+                    `components.campAdmin.campSharingSettings.${sharingStatus}.description`
+                  )
+                }}
+              </p>
+              <p>
+                <v-btn
+                  v-if="isManager"
+                  :color="camp.isShared ? '' : 'error'"
+                  elevation="0"
+                  text
+                  class="v-btn--has-bg"
+                  :loading="loading"
+                  @click="toggleShare"
+                  ><v-icon start>mdi-alert</v-icon
+                  >{{
+                    camp.isShared
+                      ? $t('components.campAdmin.campSharingSettings.deactivate')
+                      : $t('components.campAdmin.campSharingSettings.activate')
+                  }}</v-btn
+                >
+              </p>
+              <p v-if="!camp.isShared">
+                {{ $t('components.campAdmin.campSharingSettings.implications') }}
+              </p>
+              <template v-if="camp.isShared" #more-actions>
+                <div class="d-flex gap-2 align-center w-100">
+                  <small class="flex-shrink-1 flex-grow-1 w-0"
+                    ><a :href="campUrl">{{ campUrl }}</a></small
+                  >
 
-                <v-btn text class="v-btn--has-bg" @click="copyCampUrlToClipboard()">
-                  <v-icon start>mdi-clipboard-check-multiple-outline</v-icon>
-                  {{ $t('global.button.copy') }}
-                </v-btn>
-              </div>
-            </template>
-          </DialogShare>
-        </v-list-item-action>
+                  <v-btn text class="v-btn--has-bg" @click="copyCampUrlToClipboard()">
+                    <v-icon start>mdi-clipboard-check-multiple-outline</v-icon>
+                    {{ $t('global.button.copy') }}
+                  </v-btn>
+                </div>
+              </template>
+            </DialogShare>
+          </v-list-item-action>
+        </template>
       </v-list-item>
     </v-list>
   </content-group>
