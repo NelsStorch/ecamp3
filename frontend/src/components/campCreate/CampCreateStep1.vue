99b9059cf7eb3938772df81ae03fb38615ce68ad
carlobeltrame <carlo@beltra.me>
2025-10-05 13:10:02 +0200

    Disable validationobserver for now

--- frontend/src/components/campCreate/CampCreateStep1.vue
+++ frontend/src/components/campCreate/CampCreateStep1.vue
@@ -1,55 +1,55 @@
 <template>
   <v-stepper-content :step="1" class="pa-0">
-    <ValidationObserver v-slot="{ handleSubmit, valid, validate }">
-      <e-form name="camp">
-        <v-form ref="form" @submit.prevent="handleSubmit(() => $emit('next-step'))">
-          <v-card-text>
-            <e-text-field
-              v-model="localCamp.title"
-              path="title"
-              :placeholder="$t('components.campCreate.campCreateStep1.titlePlaceholder')"
-              vee-rules="required|max:32"
-              data-testid="create-camp-title-input"
-              required
-            />
-            <e-text-field
-              v-model="localCamp.organizer"
-              path="organizer"
-              data-testid="create-camp-organizer"
-            />
-            <e-text-field
-              v-model="localCamp.motto"
-              path="motto"
-              data-testid="create-camp-motto"
-            />
-            <CreateCampPeriods
-              :add-period="addPeriod"
-              :periods="localCamp.periods"
-              :delete-period="deletePeriod"
-              :period-deletable="periodDeletable"
-            />
-          </v-card-text>
-          <v-divider />
-          <ContentActions>
-            <v-spacer />
-            <ButtonCancel :disabled="isSaving" @click="$router.go(-1)" />
-            <ButtonContinue
-              v-if="valid"
-              data-testid="create-camp-next-step"
-              @click="$emit('next-step')"
-            />
-            <v-tooltip v-else location="top">
-              <template #activator="{ props }">
-                <v-btn elevation="0" color="secondary" v-bind="props" @click="validate()">
-                  {{ $t('global.button.continue') }}
-                </v-btn>
-              </template>
-              {{ $t('components.campCreate.campCreateStep1.submitTooltip') }}
-            </v-tooltip>
-          </ContentActions>
-        </v-form>
-      </e-form>
-    </ValidationObserver>
+    <!--    <ValidationObserver v-slot="{ handleSubmit, valid, validate }">-->
+    <e-form name="camp">
+      <v-form ref="form" @submit.prevent="handleSubmit(() => $emit('next-step'))">
+        <v-card-text>
+          <e-text-field
+            v-model="localCamp.title"
+            path="title"
+            :placeholder="$t('components.campCreate.campCreateStep1.titlePlaceholder')"
+            vee-rules="required|max:32"
+            data-testid="create-camp-title-input"
+            required
+          />
+          <e-text-field
+            v-model="localCamp.organizer"
+            path="organizer"
+            data-testid="create-camp-organizer"
+          />
+          <e-text-field
+            v-model="localCamp.motto"
+            path="motto"
+            data-testid="create-camp-motto"
+          />
+          <CreateCampPeriods
+            :add-period="addPeriod"
+            :periods="localCamp.periods"
+            :delete-period="deletePeriod"
+            :period-deletable="periodDeletable"
+          />
+        </v-card-text>
+        <v-divider />
+        <ContentActions>
+          <v-spacer />
+          <ButtonCancel :disabled="isSaving" @click="$router.go(-1)" />
+          <ButtonContinue
+            v-if="valid"
+            data-testid="create-camp-next-step"
+            @click="$emit('next-step')"
+          />
+          <v-tooltip v-else location="top">
+            <template #activator="{ props }">
+              <v-btn elevation="0" color="secondary" v-bind="props" @click="validate()">
+                {{ $t('global.button.continue') }}
+              </v-btn>
+            </template>
+            {{ $t('components.campCreate.campCreateStep1.submitTooltip') }}
+          </v-tooltip>
+        </ContentActions>
+      </v-form>
+    </e-form>
+    <!--</ValidationObserver>-->
   </v-stepper-content>
 </template>
 <script>
