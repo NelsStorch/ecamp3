8381f9cd71c68912a96a487356d2ff8a85adf947
BacLuc <lucius.bachmann@gmx.ch>
2025-10-25 21:49:32 +0200

    frontend: enable vue/no-v-for-template-key-on-child eslint rule

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -55,7 +55,6 @@ export default [
       'vue/multi-word-component-names': 'off',
       'vue/no-reserved-component-names': ['error', { htmlElementCaseSensitive: true }],
       'vue/no-deprecated-v-bind-sync': 'off',
-      'vue/no-v-for-template-key-on-child': 'off',
       'vue/no-v-model-argument': 'warn',
       'vue/require-explicit-emits': 'off',
 
