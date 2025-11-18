5b509e459a33a58c523d25001c3367ed723f8b60
Urban Suppiger <urban@suppiger.net>
2025-11-18 20:48:31 +0100

    revert ETextField back to options API

--- frontend/src/components/form/base/ETextField.vue
+++ frontend/src/components/form/base/ETextField.vue
@@ -1,153 +1,77 @@
 <template>
-  <div class="e-form-container">
-    <Field
-      v-slot="{ errors: veeFieldErrors }"
-      :name="veeId ?? path"
-      :label="validationLabel"
-      :rules="veeRules"
+  <Field
+    v-slot="{ handleChange, handleReset, errors: veeFieldErrors }"
+    :name="veeId ?? path"
+    :label="validationLabel"
+    :rules="veeRules"
+    class="e-form-container"
+  >
+    <v-text-field
+      ref="textField"
+      :class="[inputClass]"
+      :error-messages="(veeFieldErrors ?? []).concat(errorMessages)"
+      :label="labelOrEntityFieldLabel"
+      :type="type"
+      v-bind="$attrs"
+      :filled="filled"
+      :required="required"
+      :hide-details="hideDetails"
+      @blur="onBlur($event, handleReset)"
+      @update:model-value="onInput($event, handleChange)"
     >
-      <v-text-field
-        ref="textField"
-        :class="[inputClass]"
-        :error-messages="(veeFieldErrors ?? []).concat(errorMessages)"
-        :label="labelOrEntityFieldLabel"
-        :model-value="inputValue"
-        :type="type"
-        v-bind="$attrs"
-        :filled="filled"
-        :required="required"
-        :hide-details="hideDetails"
-        @blur="handleBlur"
-        @update:model-value="handleUpdate"
-      >
-        <!-- passing through all slots -->
-        <template v-for="(_, slot) of $slots" #[slot]="slotData">
-          <slot :name="slot" v-bind="slotData || {}"></slot>
-        </template>
-      </v-text-field>
-    </Field>
-  </div>
+      <!-- passing through all slots -->
+      <template v-for="(_, slot) of $slots" #[slot]="slotData">
+        <slot :name="slot" v-bind="slotData || {}"></slot>
+      </template>
+    </v-text-field>
+  </Field>
 </template>
 
-<script setup>
-import { inject, watch } from 'vue'
-import { Field, useField } from 'vee-validate'
-import { useI18n } from 'vue-i18n'
-
-import { props as formComponentProps, useFormComponent } from './formComponentProps'
-import {
-  props as formComponentValidationProps,
-  useValidation,
-} from './formComponentValidation'
-
-const { t } = useI18n()
-
-const entityName = inject('entityName', null)
-
-const emit = defineEmits(['input'])
-
-const props = defineProps({
-  ...formComponentProps,
-  ...formComponentValidationProps,
-  /**
-   * additional props for ETextField
-   */
-  type: {
-    type: String,
-    default: 'text',
-  },
-})
-
-const { required } = useValidation(props.veeRules)
-const { labelOrEntityFieldLabel, validationLabel } = useFormComponent(
-  props.label,
-  props.path,
-  props.validationLabelOverride,
-  entityName,
-  t
-)
-
-const {
-  value: inputValue,
-  handleBlur,
-  handleChange,
-  setValue,
-} = useField(props.veeId ?? props.path, props.veeRules, {
-  initialValue: props.modelValue,
-  label: validationLabel,
-})
-
-const handleUpdate = (value) => {
-  handleChange(value)
-  emit('update:model-value', value)
-}
-
-watch(
-  () => props.modelValue,
-  async (newValue, _) => {
-    setValue(newValue)
-  }
-)
-</script>
-
-<!-- <script>
+<script>
 import { Field } from 'vee-validate'
 import { formComponentPropsMixin } from '@/mixins/formComponentPropsMixin.js'
 import { formComponentMixin } from '@/mixins/formComponentMixin.js'
 
 export default {
   name: 'ETextField',
-  components: {
-    Field,
+  components: { Field },
+  mixins: [formComponentPropsMixin, formComponentMixin],
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
   },
   data() {
     return {
       preventValidationOnBlur: false,
     }
   },
-  computed: {
-    inputListeners: function () {
-      const vm = this
-      return Object.assign(
-        {},
-        // attach all $parent listeners
-        this.$listeners,
-        // override @input listener for correct handling of numeric values
-        {
-          input: function (value) {
-            vm.$data.preventValidationOnBlur = false
-            vm.$emit('input', value)
-          },
-          blur: function () {
-            vm.$emit('blur')
-            if (vm.$data.preventValidationOnBlur) {
-              vm.$refs.validationProvider.reset()
-            }
-            vm.$data.preventValidationOnBlur = false
-          },
-        }
-      )
-    },
-  },
   mounted() {
     this.preventValidationOnBlur =
       'autofocus' in this.$attrs &&
-      /*'required' in this.$refs.validationProvider.$attrs && */
+      'required' in this.$refs.validationProvider.$attrs &&
       this.$refs.textField.value == ''
   },
   methods: {
     focus() {
       this.$refs.textField.focus()
     },
-    isRequired(value) {
-      if (value && value.trim()) {
-        return true
+    onInput(event, handleChange) {
+      this.preventValidationOnBlur = false
+      handleChange(event)
+      this.$emit('update:model-value', event)
+    },
+    onBlur(event, handleReset) {
+      this.$emit('blur', event)
+      if (this.preventValidationOnBlur) {
+        handleReset()
       }
-      return 'This is required'
+      this.preventValidationOnBlur = false
     },
   },
 }
-</script> -->
+</script>
 
 <style scoped>
 [required]:deep(label::after) {
