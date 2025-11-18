46126fa52e7d113775268be7e438fec4cff7d75b
Samuel Imfeld <samuel.imfeld@autodesk.com>
2025-11-18 23:26:34 +0100

    fix model-value prop naming
    apiPropsMixin defines a modelValue prop which overrides model-value when destructuring with v-bind="{ ...$props, ...$attrs }"

--- frontend/src/components/form/api/ApiRichtext.vue
+++ frontend/src/components/form/api/ApiRichtext.vue
@@ -6,7 +6,7 @@ Displays a field as a e-textarea + write access via API wrapper
   <api-wrapper v-slot="wrapper" v-bind="{ ...$props, ...$attrs }" separate-buttons>
     <e-richtext
       :model-value="wrapper.localValue"
-      v-bind="{ ...$props, ...$attrs }"
+      v-bind="$attrs"
       :path="path"
       :readonly="wrapper.readonly"
       :disabled="disabled"
