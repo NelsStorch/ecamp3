e8994f345cad2499ac12be7d83a2d288f7be8bf9
BacLuc <lucius.bachmann@gmx.ch>
2025-12-21 17:06:16 +0100

    frontend: fix lint for ECheckbox.spec.js

--- frontend/src/components/form/base/__tests__/ECheckbox.spec.js
+++ frontend/src/components/form/base/__tests__/ECheckbox.spec.js
@@ -1,4 +1,4 @@
-import { describe, expect, test, vi } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { setupVuetify } from '/tests/setupVuetify.js'
 import { mount as mountComponent } from '@vue/test-utils'
 import ECheckbox from '../ECheckbox.vue'
