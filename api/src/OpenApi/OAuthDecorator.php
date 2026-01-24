1a8c5b6b6600a9678371b5d27e9284c02b4f4e4f
BacLuc <lucius.bachmann@gmx.ch>
2026-01-24 17:52:55 +0100

    api: fix lint after updating api-platform packages

--- api/src/OpenApi/OAuthDecorator.php
+++ api/src/OpenApi/OAuthDecorator.php
@@ -3,7 +3,10 @@
 namespace App\OpenApi;
 
 use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
-use ApiPlatform\OpenApi\Model;
+use ApiPlatform\OpenApi\Model\Operation;
+use ApiPlatform\OpenApi\Model\Parameter;
+use ApiPlatform\OpenApi\Model\PathItem;
+use ApiPlatform\OpenApi\Model\Response;
 use ApiPlatform\OpenApi\OpenApi;
 
 /**
@@ -15,19 +18,19 @@ final readonly class OAuthDecorator implements OpenApiFactoryInterface {
     public function __invoke(array $context = []): OpenApi {
         $openApi = ($this->decorated)($context);
 
-        $pathItemGoogle = new Model\PathItem(
+        $pathItemGoogle = new PathItem(
             ref: 'Google OAuth',
-            get: new Model\Operation(
+            get: new Operation(
                 operationId: 'oauthGoogleRedirect',
                 tags: ['OAuth'],
                 responses: [
-                    '302' => [
-                        'description' => 'Redirect to the Google OAuth authorization endpoint',
-                    ],
+                    '302' => new Response(
+                        description: 'Redirect to the Google OAuth authorization endpoint',
+                    ),
                 ],
                 summary: 'Log in using Google Oauth.',
                 parameters: [
-                    new Model\Parameter(
+                    new Parameter(
                         name: 'callback',
                         in: 'path',
                         schema: [
@@ -39,19 +42,19 @@ final readonly class OAuthDecorator implements OpenApiFactoryInterface {
         );
         $openApi->getPaths()->addPath('/auth/google', $pathItemGoogle);
 
-        $pathItemPbsmidata = new Model\PathItem(
+        $pathItemPbsmidata = new PathItem(
             ref: 'PBS MiData OAuth',
-            get: new Model\Operation(
+            get: new Operation(
                 operationId: 'oauthPbsmidataRedirect',
                 tags: ['OAuth'],
                 responses: [
-                    '302' => [
-                        'description' => 'Redirect to the PBS MiData OAuth authorization endpoint',
-                    ],
+                    '302' => new Response(
+                        description: 'Redirect to the PBS MiData OAuth authorization endpoint',
+                    ),
                 ],
                 summary: 'Log in using PBS MiData Oauth.',
                 parameters: [
-                    new Model\Parameter(
+                    new Parameter(
                         name: 'callback',
                         in: 'path',
                         schema: [
@@ -63,19 +66,19 @@ final readonly class OAuthDecorator implements OpenApiFactoryInterface {
         );
         $openApi->getPaths()->addPath('/auth/pbsmidata', $pathItemPbsmidata);
 
-        $pathItemCevidb = new Model\PathItem(
+        $pathItemCevidb = new PathItem(
             ref: 'CeviDB OAuth',
-            get: new Model\Operation(
+            get: new Operation(
                 operationId: 'oauthCevidbRedirect',
                 tags: ['OAuth'],
                 responses: [
-                    '302' => [
-                        'description' => 'Redirect to the CeviDB OAuth authorization endpoint',
-                    ],
+                    '302' => new Response(
+                        description: 'Redirect to the CeviDB OAuth authorization endpoint',
+                    ),
                 ],
                 summary: 'Log in using CeviDB Oauth.',
                 parameters: [
-                    new Model\Parameter(
+                    new Parameter(
                         name: 'callback',
                         in: 'path',
                         schema: [
@@ -87,19 +90,19 @@ final readonly class OAuthDecorator implements OpenApiFactoryInterface {
         );
         $openApi->getPaths()->addPath('/auth/cevidb', $pathItemCevidb);
 
-        $pathItemJubladb = new Model\PathItem(
+        $pathItemJubladb = new PathItem(
             ref: 'JublaDB OAuth',
-            get: new Model\Operation(
+            get: new Operation(
                 operationId: 'oauthJubladbRedirect',
                 tags: ['OAuth'],
                 responses: [
-                    '302' => [
-                        'description' => 'Redirect to the JublaDB OAuth authorization endpoint',
-                    ],
+                    '302' => new Response(
+                        description: 'Redirect to the JublaDB OAuth authorization endpoint',
+                    ),
                 ],
                 summary: 'Log in using JublaDB Oauth.',
                 parameters: [
-                    new Model\Parameter(
+                    new Parameter(
                         name: 'callback',
                         in: 'path',
                         schema: [
