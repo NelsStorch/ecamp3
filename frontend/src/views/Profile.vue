316fc9e4d6bbf0be36e84bcb25fb871bc6a7633b
BacLuc <lucius.bachmann@gmx.ch>
2026-02-08 13:47:35 +0100

    Profile: revert commit 5ce79ab7
    This was clearly not tested.

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -110,7 +110,7 @@ export default {
     availableLocales() {
       return VueI18n.global.availableLocales.map((l) => ({
         value: l,
-        text: this.$t('global.language', { locale: l }, 1),
+        text: this.$t('global.language', 1, { locale: l }),
       }))
     },
   },
