516ec3557eddd55d473678904d3ad29273a697a7
carlobeltrame <carlo@beltra.me>
2026-04-10 23:13:03 +0200

    Remove overly complicated n+1-prone frontend logic
    This logic to calculate the activities which still use a checklist node
    has led to race conditions and n+1 requests multiple times in the past,
    and it looks like it might be causing another n+1 sometimes, which
    causes a memory leak and freezes the page.

--- frontend/src/components/checklist/ChecklistItemEdit.vue
+++ frontend/src/components/checklist/ChecklistItemEdit.vue
@@ -14,19 +14,12 @@
     <template #moreActions>
       <PromptEntityDelete
         :entity="checklistItem"
-        :submit-enabled="activitiesWithChecklistItem.length === 0"
         align="left"
         position="top"
         :btn-attrs="{
           class: 'v-btn--has-bg',
         }"
       >
-        <template v-if="activitiesWithChecklistItem.length > 0" #error>
-          <ErrorExistingActivitiesList
-            :camp="camp"
-            :existing-activities="activitiesWithChecklistItem"
-          />
-        </template>
         {{
           $t(
             'components.checklist.checklistItemEdit.delete',
@@ -52,18 +45,15 @@
 </template>
 
 <script>
-import { sortBy } from 'lodash-es'
 import DetailPane from '@/components/generic/DetailPane.vue'
 import DialogBase from '@/components/dialog/DialogBase.vue'
 import PromptEntityDelete from '@/components/prompt/PromptEntityDelete.vue'
-import ErrorExistingActivitiesList from '@/components/campAdmin/ErrorExistingActivitiesList.vue'
 
 export default {
   name: 'ChecklistItemEdit',
   components: {
     DetailPane,
     PromptEntityDelete,
-    ErrorExistingActivitiesList,
   },
   extends: DialogBase,
   provide() {
@@ -79,7 +69,6 @@ export default {
     return {
       entityProperties: ['checklist', 'text'],
       entityUri: '',
-      activitiesWithChecklistItem: [],
     }
   },
   computed: {
@@ -99,57 +88,9 @@ export default {
         this.clearEntityData()
       }
     },
-    checklistItem: {
-      async handler(checklistItem) {
-        await this.camp.activities()._meta.load
-        const activities = await Promise.all(
-          this.camp.activities().items.map(async (a) => ({
-            activity: a,
-            rootContentNodeUri: await a.$href('rootContentNode'),
-          }))
-        )
-
-        await this.loadData()
-
-        const checklistNodes = await Promise.all(
-          checklistItem.checklistNodes().items.map(async (cn) => ({
-            checklistNode: cn,
-            rootUri: await cn.$href('root'),
-          }))
-        )
-
-        const res = sortBy(
-          activities
-            .filter((a) =>
-              checklistNodes.some((cn) => cn.rootUri == a.rootContentNodeUri)
-            )
-            .map((a) => a.activity),
-          (activity) =>
-            activity
-              .scheduleEntries()
-              .items.map(
-                (s) =>
-                  `${s.dayNumber}`.padStart(3, '0') +
-                  `${s.scheduleEntryNumber}`.padStart(3, '0')
-              )
-              .reduce((p, v) => (p < v ? p : v))
-        )
-        this.activitiesWithChecklistItem = res
-      },
-      immediate: true,
-    },
   },
   async mounted() {
     this.api.href(this.api.get(), 'checklistItems').then((uri) => (this.entityUri = uri))
-    this.loadData()
-  },
-  methods: {
-    async loadData() {
-      return Promise.all([
-        this.api.get().checklistNodes({ camp: this.camp._meta.self }),
-        this.api.get().checklistItems({ 'checklist.camp': this.camp._meta.self }),
-      ])
-    },
   },
 }
 </script>
