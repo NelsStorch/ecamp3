b295c677488e9074bc88f50b3b1a1f1f938e1a8b
Pirmin Mattmann <pimattmann@gmail.com>
2026-04-11 13:41:59 +0200

    DB-index, sync ContentNode and Migration

--- api/migrations/schema/Version20260411073822.php
+++ api/migrations/schema/Version20260411073822.php
@@ -17,7 +17,7 @@ final class Version20260411073822 extends AbstractMigration {
 
     public function up(Schema $schema): void {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE INDEX IDX_481D0580BF396750 ON content_node (id) WHERE strategy = \'checklistnode\'');
+        $this->addSql('CREATE INDEX IDX_481D0580BF396750 ON content_node (id) WHERE ((strategy)::text = \'checklistnode\'::text)');
         $this->addSql('CREATE INDEX unmanaged_idx_checklistnode_checklistitem_node_item ON checklistnode_checklistitem (checklistnode_id, checklistitem_id)');
     }
 
