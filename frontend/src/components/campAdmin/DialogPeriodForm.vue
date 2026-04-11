5068de3860e2059556a6ad0aa515eccb62fc8f61
Urban Suppiger <449555+usu@users.noreply.github.com>
2026-04-11 09:40:43 +0200

    re-enable validation in period form

--- frontend/src/components/campAdmin/DialogPeriodForm.vue
+++ frontend/src/components/campAdmin/DialogPeriodForm.vue
@@ -14,12 +14,12 @@
       :max="localPeriod.end"
       @update:model-value="localPeriod.start = $event"
     />
-
-    <!-- vee-rules="required|greaterThanOrEqual_date:@start" -->
+    
     <e-date-picker
       :min="localPeriod.start"
       :model-value="localPeriod.end"
       path="end"
+      vee-rules="required|greaterThanOrEqual_date:@start"
       @update:model-value="localPeriod.end = $event"
     />
   </e-form>
