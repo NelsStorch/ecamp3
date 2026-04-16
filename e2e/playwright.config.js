7a43f902a60beb87244455a26f46386b07e69312
Manuel Meister <news.manuelsworld@gmail.com>
2026-04-16 14:46:34 +0200

    Merge playwright reports

--- e2e/playwright.config.js
+++ e2e/playwright.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: process.env.CI ? 'blob' : 'html',
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
